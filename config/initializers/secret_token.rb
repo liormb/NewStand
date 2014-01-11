4196f564e3ebf7fc1108e4edd7bc50d204a2cc1e
liormb <liormb@gmail.com>
2014-01-11 08:46:57 -0500

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newstand::Application.config.secret_token = '91dc1282dd40b40fb926bfec6740cfd36a7b0d9c424186eb20f3aa61b08bafd5cb4d6c6de251b8cd740d6ec1f17844a9599497d3de0104b64c3956ee91704dc1'
